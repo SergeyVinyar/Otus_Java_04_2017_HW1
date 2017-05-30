a6a3b6b9c1a3c0a72865a658e8394b99c8880074
Sergey Vinyarsky <sergey@vinyarsky.ru>
2017-05-30 22:24:02 +0300

    Errors fixed

--- hw8/src/main/java/ru/vinyarsky/hw8/JsonWriter.java
+++ hw8/src/main/java/ru/vinyarsky/hw8/JsonWriter.java
@@ -50,18 +50,13 @@ public class JsonWriter {
         Class clazz = input.getClass();
         if (isSimpleObject(input)) {
             JSONObject json = new JSONObject();
-            json.put("value", input);
+            addToJson(json, "value", input);
             result = json;
         }
         else if (clazz.isArray()) {
             JSONArray json = new JSONArray();
-            for (int i = 0; i < Array.getLength(input); i++) {
-                Object value = Array.get(input, i);
-                if (isSimpleObject(value))
-                    json.add(value);
-                else
-                    json.add(processObject(value));
-            }
+            for (int i = 0; i < Array.getLength(input); i++)
+                addToJson(json, Array.get(input, i));
             result = json;
         }
         else {
@@ -69,52 +64,51 @@ public class JsonWriter {
             if (inputInterfaces.contains(List.class) || inputInterfaces.contains(Set.class)) {
                 JSONArray json = new JSONArray();
                 Collection collection = (Collection) input;
-                for (Object value : collection) {
-                    if (isSimpleObject(value))
-                        json.add(value);
-                    else
-                        json.add(processObject(value));
-                }
+                for (Object value : collection)
+                    addToJson(json, value);
                 result = json;
             } else if (inputInterfaces.contains(Map.class)) {
                 JSONObject json = new JSONObject();
-                for (Object entry : ((Map) input).entrySet()) {
-                    Object key = ((Map.Entry) entry).getKey();
-                    Object value = ((Map.Entry) entry).getValue();
-                    if (isSimpleObject(value))
-                        json.put(key, value);
-                    else
-                        json.put(key, processObject(value));
-                }
+                for (Object entry : ((Map) input).entrySet())
+                    addToJson(json, ((Map.Entry) entry).getKey(), ((Map.Entry) entry).getValue());
                 result = json;
             } else {
                 JSONObject json = new JSONObject();
-                for(Field field : clazz.getFields()) {
-                    Object value = field.get(input);
-                    if (isSimpleObject(value))
-                        json.put(field.getName(), value);
-                    else
-                        json.put(field.getName(), processObject(value));
-                }
+                for(Field field : clazz.getFields())
+                    addToJson(json, field.getName(), field.get(input));
                 result = json;
             }
         }
         return result;
     }
 
+    private static void addToJson(JSONArray jsonArray, Object value) throws Exception {
+        if (isSimpleObject(value))
+            jsonArray.add(value);
+        else
+            jsonArray.add(processObject(value));
+    };
+
+    private static void addToJson(JSONObject jsonObject, Object key, Object value) throws Exception {
+        if (isSimpleObject(value))
+            jsonObject.put(key, value);
+        else
+            jsonObject.put(key, processObject(value));
+    };
+
     private static boolean isSimpleObject(Object input) {
         if (input == null)
             return true;
 
         Class clazz = input.getClass();
-        return     clazz.equals(Long.class)      || clazz.equals(long.class)
-                || clazz.equals(Integer.class)   || clazz.equals(int.class)
-                || clazz.equals(Short.class)     || clazz.equals(short.class)
-                || clazz.equals(Byte.class)      || clazz.equals(byte.class)
-                || clazz.equals(Float.class)     || clazz.equals(float.class)
-                || clazz.equals(Double.class)    || clazz.equals(double.class)
-                || clazz.equals(Character.class) || clazz.equals(char.class)
-                || clazz.equals(Boolean.class)   || clazz.equals(boolean.class)
+        return     clazz.equals(Long.class)
+                || clazz.equals(Integer.class)
+                || clazz.equals(Short.class)
+                || clazz.equals(Byte.class)
+                || clazz.equals(Float.class)
+                || clazz.equals(Double.class)
+                || clazz.equals(Character.class)
+                || clazz.equals(Boolean.class)
                 || clazz.equals(String.class)
                 || clazz.equals(Object.class);
     }
